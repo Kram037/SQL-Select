2f8004ed8cc53234898d30fa309bb58d5e78c897
Marco De Fenzo <82841719+Kram037@users.noreply.github.com>
2022-12-13 12:47:22 +0100

    Update Start.java

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Start {
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+
+        try{
+            String url = "jdbc:mysql://localhost:3306/newdb";
+            String user = "root";
+            String password = "PinoDaniele03";
+
+            con = DriverManager.getConnection(url, user, password);
+
+            Statement s = con.createStatement();
+
+            String sql = "CREATE TABLE student " +
+                    "(student_id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    " last_name VARCHAR(30), " +
+                    " first_name VARCHAR(30))";
+
+            ArrayList<String> surnames = new ArrayList<>();
+
+            s.executeUpdate(sql);
+
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('De Fenzo', 'Marco');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Vassarotti', 'Vittoria');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Di Leo', 'Luca');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Mustata', 'Alina Elena');");
+
+            ResultSet rS = s.executeQuery("SELECT last_name, first_name FROM newdb.student;");
+
+            while(rS.next()){
+                surnames.add(rS.getString("last_name"));
+                System.out.println(rS.getString("first_name"));
+            }
+
+            System.out.println(surnames);
+
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            try{
+                if(con != null)
+                    con.close();
+            }catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
